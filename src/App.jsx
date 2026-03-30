2089f0e00f56655c34ae46e23f0033d85ecc038f
Aleksandr <aleksandr_ermakov@outlook.com>
2026-03-30 22:52:31 +0300

    useEffect middle

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 import "./App.css";
+import { UseEffect } from "./components/UseEffect";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Alena server request</h1>
+      <UseEffect />
+      {/* <h1>Alena server request</h1> */}
     </>
   );
 }
---
+++ src/components/UseEffect.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+export const UseEffect = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("https://mocki.io/v1/4fda5797-2f13-4125-a859-e29fa8fc0c5c")
+      .then((loadedData) => loadedData.json())
+      .then((loadedProducts) => {
+        setProducts(loadedProducts);
+      });
+  }, []);
+
+  return (
+    <>
+      <h2>alena</h2>
+      {products.map(({ id, name, price }) => {
+        return (
+          <div key={id}>
+            {name} - {price}
+          </div>
+        );
+      })}
+    </>
+  );
+};
