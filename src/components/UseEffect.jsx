57dbc77328511ade3eaa4e701fedc018f08291aa
Aleksandr <aleksandr_ermakov@outlook.com>
2026-03-31 14:08:38 +0300

    test

--- src/components/UseEffect.jsx
+++ src/components/UseEffect.jsx
@@ -12,7 +12,7 @@ export const UseEffect = () => {
 
   return (
     <>
-      <h2>Alena</h2>
+      <h2>Alena!</h2>
       {products.map(({ id, name, price }) => {
         return (
           <div key={id}>
